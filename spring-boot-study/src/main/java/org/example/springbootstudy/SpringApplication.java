522c53bc4f723c0ca84ca32867fe1f2983d76979
Sim Wook <wook.sim@gmo.media>
2025-06-30 19:27:11 +0900

    infra bean情報分離

--- spring-boot-study/src/main/java/org/example/springbootstudy/SpringApplication.java
+++ spring-boot-study/src/main/java/org/example/springbootstudy/SpringApplication.java
@@ -1,24 +1,12 @@
 package org.example.springbootstudy;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.example.springbootstudy.config.MySpringBootApplication;
 
 import static org.springframework.boot.SpringApplication.run;
 
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class SpringApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         run(SpringApplication.class, args);
